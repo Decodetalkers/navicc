408bc0f54e48f6902a7f0b410ac2b216d2223a10
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-11-11 22:47:16 +0900

    chore: base footer

--- components/foot.tsx
+++ components/foot.tsx
@@ -1,4 +1,11 @@
-import { CopyRight, FootSession } from "~/styles/foot.ts";
+import {
+  CopyRight,
+  FootSession,
+  SNSBtn,
+  SNSPart,
+  SNSPartInner,
+} from "~/styles/foot.ts";
+import { PropsWithChildren } from "react";
 
 export function Foot() {
   return (
@@ -9,7 +16,56 @@ export function Foot() {
             Copyright(C) NAVIC Co.,LTD. All Rights Reserved.
           </h5>
         </CopyRight>
+        <SNSPart>
+          <SNSPartInner>
+            <SNSButton
+              img="static/youtube.svg"
+              url="https://www.youtube.com/channel/UCgWqi12hhPgunsA2_-U9pGg"
+            >
+              NAVIC<br />公式チャンネル
+            </SNSButton>
+            <SNSButton
+              img="static/youtube.svg"
+              url="https://www.youtube.com/channel/UCvsr7nZnUU0BnzITiSD-fpg"
+            >
+              CLEZEED<br />
+              公式チャンネル
+            </SNSButton>
+            <SNSButton
+              img="static/youtube.svg"
+              url="https://www.youtube.com/channel/UCvsr7nZnUU0BnzITiSD-fpg"
+            >
+              豊橋<br />
+              モーターズ
+            </SNSButton>
+            <SNSButton
+              img="static/tiktok.svg"
+              url="https://www.tiktok.com/@navic.inc?_t=ZS-8tJwnPeJqDI&_r=1"
+            >
+              NAVIC公式<br />
+              TikTok
+            </SNSButton>
+          </SNSPartInner>
+        </SNSPart>
       </FootSession>
     </footer>
+  );
+}
+
+type SNSBaseInfo = {
+  img: string;
+  url: string;
+};
+
+function SNSButton({ img, url, children }: PropsWithChildren<SNSBaseInfo>) {
+  return (
+    <SNSBtn>
+      <a href={url}>
+        <img src={img} />
+        <span>
+          {children}
+        </span>
+      </a>
+    </SNSBtn>
   );
 }
--- styles/foot.ts
+++ styles/foot.ts
@@ -17,3 +17,49 @@ export const CopyRight = styled.div`
     margin: 6px;
   }
 `;
+
+export const SNSPart = styled.div`
+  display: flex;
+  flex-direction: flow;
+  justify-content: center;
+  background-color: #053769ff;
+  gap: 10%;
+`;
+export const SNSPartInner = styled.div`
+  display: flex;
+  flex-direction: flow;
+  justify-content: center;
+  gap: 10%;
+  width: 70%;
+`;
+export const SNSBtn = styled.span`
+  margin-top: 50px;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  width: 120px;
+  height: 120px;
+  position: relative;
+  cursor: pointer;
+  & a {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-decoration: none;
+    color: white;
+    font-size: 12px;
+    text-align: center;
+  }
+  & img {
+    width: 50px;
+    transition: width 1s 0.1s;
+  }
+  &:hover {
+    border-radius: 30%;
+    background-color: #14508cff;
+    & img {
+      width: 100px;
+    }
+  }
+`;
